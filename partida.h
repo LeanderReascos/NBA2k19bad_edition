ac6abc96c747b23757c62c1a96bdf51f63121a99
magdamd <49106367+magdamd@users.noreply.github.com>
2019-04-03 20:15:47 +0100

    Add files via upload
    função NEWGAME-gera uma nova partida e carrega os dados para a estrutura

---
+++ partida.h
@@ -0,0 +1,62 @@
+
+typedef struct partida{
+	int njogadores;
+	CAMPO  campo;
+	
+}PARTIDA;
+
+
+typedef struct planeta{
+	char nome[8];
+	float lcampo;
+	float acampo;
+	float g;
+	
+}PLANETA;
+
+void NEWGAME(PARTIDA* partidanova ,int opcao){
+
+
+PLANETA* planeta;
+planeta=(PLANETA*) malloc(sizeof(PLANETA));
+
+
+partidanova->campo.lChar=128;
+partidanova->campo.aChar=50;
+strcpy(partidanova->campo.file,"campo");
+
+	
+printf ("How many players will play this round?");
+scanf("%d",&(partidanova->njogadores));
+
+FILE* fp;
+fp=fopen("planets.data","r");
+if(fp==NULL){
+	printf("ERROR\n");
+	exit(-1);
+} 
+//fread(planeta,sizeof(PLANETA),opcao,fp);
+//ler o planeta-NOVA MANEIRA
+
+int i=0;
+do{
+	i++;
+	fread(planeta,sizeof(PLANETA),1,fp);
+	
+	
+}while(i!=opcao && i<=9);
+
+
+partidanova->campo.g=planeta->g;
+partidanova->campo.ancho=planeta->acampo;
+partidanova->campo.largo=planeta->lcampo;
+strcpy(partidanova->campo.nome,planeta->nome);
+strcpy(partidanova->campo.file,"campo");
+
+
+//AQUI NAO ACABOU DE CORRER
+readCampo( &(partidanova->campo));
+
+}
+
+
