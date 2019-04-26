c778bdf14dd19c731e47be71a792585d90c3428b
LeanderReascos <49106116+LeanderReascos@users.noreply.github.com>
2019-04-26 02:19:13 +0100

    python

---
+++ game.py
@@ -0,0 +1,93 @@
+import pygame
+import sys
+import random
+
+path = "MUSICAS\\"
+
+def pmusic(pos,nmusics,files):
+    pygame.init()
+    pygame.mixer.init()
+    clock = pygame.time.Clock()
+    pygame.mixer.music.load(path+files[pos])
+    pygame.mixer.music.play()
+    aux = 0;
+    while pygame.mixer.music.get_busy():
+       #print("Playing...")
+        #if aux % 2 == 0:
+        readkey(path+".commands",pos,nmusics,files)
+        clock.tick(1)
+        #aux+=1
+    nextMusic(pos,nmusics,files)
+
+def readkey(keys,pos,nmusics,files):
+     f = open(keys,"r")
+     lin = f.readlines()
+     for x in lin:
+         if x == "pause":
+               pygame.mixer.music.pause()
+         elif x == "play":
+              pygame.mixer.music.unpause()
+         elif x == "stop":
+              stopmusic()
+         elif x == "next":
+              nextMusic(pos,nmusics,files)
+         elif x == "before":
+              beforeMusic(pos,nmusics,files)
+     f.close()
+def stopmusic():
+    pygame.mixer.music.stop()
+    sys.exit()
+
+def psound(file):
+    pygame.init()
+    pygame.mixer.init()
+    clock = pygame.time.Clock()
+    crash_sound = pygame.mixer.Sound(file)
+    crash_sound.play()
+    pygame.mixer.music.stop()
+    pygame.event.wait()
+
+def nextMusic(pos,nmusics,files):
+    if pos == nmusics:
+        pos = 0
+    else:
+        pos +=  1
+    f = open(path+".commands","w")
+    f.write("play")
+    f.close()
+    randomPlay(pos,nmusics,files)
+
+def beforeMusic(pos,nmusics,files):
+    if pos == 0:
+        pos = nmusics
+    else:
+        pos -=  1
+    f = open(path+".commands","w")
+    f.write("play")
+    f.close()
+    randomPlay(pos,nmusics,files)
+
+def randomPlay(pos,nmusics,files):
+    pmusic(pos,nmusics,files)
+
+def getmixerargs():
+    pygame.mixer.init()
+    freq, size, chan = pygame.mixer.get_init()
+    return freq, size, chan
+
+
+def initMixer():
+    BUFFER = 3072  # audio buffer size, number of samples since pygame 1.8.
+    FREQ, SIZE, CHAN = getmixerargs()
+    pygame.mixer.init(FREQ, SIZE, CHAN, BUFFER)
+
+
+initMixer()
+files = ["m.mp3","m1.mp3","m2.mp3","m3.mp3","m4.mp3","m5.mp3"]
+random.shuffle(files)
+pos = 0
+nmusics = 5
+randomPlay(pos,nmusics,files)
+
+   
+    
---
+++ video.py
@@ -0,0 +1,22 @@
+from moviepy.editor import *
+import pygame
+import os
+path = "VIDEOS\\"
+
+def playVideo(video):
+	pygame.display.set_caption('NBA2K19_BadEdition')
+
+	clip = VideoFileClip(video)
+	clip.preview()
+
+	pygame.quit()
+
+f = open(path+".videos","r")
+lin = f.readlines()
+for x in lin:
+	try:
+		playVideo(path+x+".mp4")
+	except Exception as e:
+		playVideo(path+"exellent.mp4")
+	
+f.close()
